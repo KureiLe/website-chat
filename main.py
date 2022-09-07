a9ee478e2b003b63435016812bdabe4fdd002a63
クレイ <83293295+KureiLe@users.noreply.github.com>
2022-09-08 02:25:11 +0700

    ss

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+    
+submitted = []
+
+@app.route('/', methods=['POST', 'GET'])
+def form():
+    if request.method == "POST":
+        text = request.form["tTXT"]
+        name = request.form["tNM"]
+        submitted.append([[str(name)], [str(text)]])
+        return render_template("index.html", submitted=submitted)
+    else:
+        return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
