dc300de239268a6e4e59c619e7fb680a10614b98
クレイ <83293295+KureiLe@users.noreply.github.com>
2022-09-10 22:50:55 +0700

    dds
    ds

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template, request
-app = Flask(__name__)
-    
-submitted = []
+from flask_socketio import SocketIO, send
 
-@app.route('/', methods=['POST', 'GET'])
-def form():
-    if request.method == "POST":
-        text = request.form["tTXT"]
-        name = request.form["tNM"]
-        submitted.append([[str(name)], [str(text)]])
-        return render_template("index.html", submitted=submitted)
-    else:
-        return render_template("index.html", submitted=submitted)
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "fhkjdshfsadkfhsdfhjklqfhjdkshflkjadfhajkdfhjksadjhfkaslfhakhdjk"
+socket = SocketIO(app)
+
+@socket.on("message")
+def handle_message(message):
+    if message != "someone connected!":
+        send(message, broadcast=True)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socket.run(app)
